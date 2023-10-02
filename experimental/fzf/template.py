981abbe8f15febaac4188a5a59f51b619b54021b
zenobit <zen@osowoso.xyz>
2023-10-02 10:10:47 +0200

    experimental/fzf: New package (0.42.0)

---
+++ experimental/fzf/template.py
@@ -0,0 +1,10 @@
+pkgname = "fzf"
+pkgver = "0.42.0"
+pkgrel = 0
+hostmakedepends = ["go"]
+pkgdesc = "Command-line fuzzy finder"
+maintainer = "zenobit <zen@duck.com>"
+license = "MIT"
+url = "https://github.com/junegunn/fzf"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "bfa36bf75e9a1e9cbbdb9abcf9d1707e479bd3a07880a8ae3564caee5711cb99"
